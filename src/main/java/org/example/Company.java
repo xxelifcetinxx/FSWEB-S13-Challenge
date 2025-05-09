917367f5c6b461b6a9c8d2b99e688b48a1d98d45
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-09 14:00:08 +0300

    Company.java ok

--- src/main/java/org/example/Company.java
+++ src/main/java/org/example/Company.java
@@ -1,4 +1,78 @@
 package org.example;
 
 public class Company {
+    private int id;
+    private String name;
+    private double giro;
+    private String[] developerNames;
+
+    public Company(int id, String name, double giro, String[] developerNames) {
+        this.id = id;
+        this.name = name;
+        setGiro(giro);
+        this.developerNames = developerNames;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGiro() {
+        return giro;
+    }
+
+    public void setGiro(double giro) {
+        if (giro < 0) {
+            this.giro = 0;
+        } else {
+            this.giro = giro;
+        }
+    }
+
+    public String[] getDeveloperNames() {
+        return developerNames;
+    }
+
+    public void setDeveloperNames(String[] developerNames) {
+        this.developerNames = developerNames;
+    }
+
+    public void addEmployee(int index, String name) {
+        if (index < 0 || index >= developerNames.length) {
+            System.out.println("Index out of bounds!");
+            return;
+        }
+        if (developerNames[index] == null) {
+            developerNames[index] = name;
+            System.out.println("Developer added at index " + index);
+        } else {
+            System.out.println("Index already occupied!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder devs = new StringBuilder();
+        for (String dev : developerNames) {
+            devs.append(dev).append(" ");
+        }
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", giro=" + giro +
+                ", developerNames=" + devs +
+                '}';
+    }
 }
