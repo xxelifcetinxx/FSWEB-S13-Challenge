d26719f8fdf6e9679430fe56d4afb9dcf8ffde3f
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-09 13:58:53 +0300

    Employee.java ok

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,84 @@
 package org.example;
 
 public class Employee {
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String[] healthplans;
+
+    public Employee(int id, String fullName, String email, String password, String[] healthplans) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.healthplans = healthplans;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getHealthplans() {
+        return healthplans;
+    }
+
+    public void setHealthplans(String[] healthplans) {
+        this.healthplans = healthplans;
+    }
+
+    public void addHealthplan(int index, String name) {
+        if (index < 0 || index >= healthplans.length) {
+            System.out.println("Index out of bounds!");
+            return;
+        }
+        if (healthplans[index] == null) {
+            healthplans[index] = name;
+            System.out.println("Healthplan added at index " + index);
+        } else {
+            System.out.println("Index already occupied!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder plans = new StringBuilder();
+        for (String hp : healthplans) {
+            plans.append(hp).append(" ");
+        }
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", healthplans=" + plans +
+                '}';
+    }
 }
